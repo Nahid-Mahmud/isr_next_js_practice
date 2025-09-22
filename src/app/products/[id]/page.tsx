d8bc01c1ee655e3916f67573a9ee73a1a0ce497c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-22 16:23:41 +0600

    feat: enhance product fetching with revalidation and caching tags

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,12 @@
 import ProductCard from "../../../components/ProductCard";
 
 export default async function page({ params }: { params: { id: string } }) {
-  const res = await fetch(`http://localhost:5000/products/${params.id}`);
+  const res = await fetch(`http://localhost:5000/products/${params.id}`, {
+    next: {
+      revalidate: 30,
+      tags: [`products`],
+    },
+  });
   const product = await res.json();
   console.log(product);
   return (
